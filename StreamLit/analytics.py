39544ffc2857bac1709e8a640fd6dec1ab82dcc5
Pythontp <pythontp8@gmail.com>
2023-05-31 14:30:42 +0530

    plotted the pie-chart

--- StreamLit/analytics.py
+++ StreamLit/analytics.py
@@ -6,39 +6,59 @@ import streamlit as st
 from django.http import request
 import streamlit as st
 
-col1, col2= st.columns(2)
+# col1, col2= st.columns(2)
 
-data = {
-            "title": "Streamlit Request",
-            "body": "This is a sample request",
-            "userId": 1
-        }
-with col1:
-    if st.button('Post'):
-        response_post = requests.post("http://127.0.0.1:8000/analytics", data=data)
-        if response_post.status_code == 200:
-            st.write("POST request successful!")
-            st.write(data)
-        else:
-            st.write(f"POST request failed! {response_post.status_code}")
-    else:
-        st.write('click here')
+# data = {
+#             "title": "Streamlit Request",
+#             "body": "This is a sample request",
+#             "userId": 1
+#         }
+# with col1:
+#     if st.button('Post'):
+#         response_post = requests.post("http://127.0.0.1:8000/analytics", data=data)
+#         if response_post.status_code == 200:
+#             st.write("POST request successful!")
+#             st.write(data)
+#         else:
+#             st.write(f"POST request failed! {response_post.status_code}")
+#     else:
+#         st.write('click here')
 
-with col2:
-    a=st.button
-    if a('get'):
-        response_get = requests.get("http://127.0.0.1:8000/analytics")
-        if response_get.status_code == 200:
-            st.write("GET request successful!")
-            st.write("Response:")
-            st.write(response_get.json())
+# with col2:
+#     a=st.button
+#     if a('get'):
+#         response_get = requests.get("http://127.0.0.1:8000/analytics")
+#         if response_get.status_code == 200:
+#             st.write("GET request successful!")
+#             st.write("Response:")
+#             st.write(response_get.json())
             
             
-        else:
-            st.write(f"GET request failed! {response_get.status_code}")
-    else:
-        st.write('click here')
+#         else:
+#             st.write(f"GET request failed! {response_get.status_code}")
+#     else:
+#         st.write('click here')
 
+
+st.header("PIE Chart")
+    #  User input form
+url = "http://127.0.0.1:8000/analytics/?type=pie"
+response = requests.get(url)
+if response.status_code == 200:
+    # Extract the data from the response
+    data = response.json()
+    labels = data['labels']
+    sizes = data['sizes']
+    # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+    
+    explode = [0,0,0,0,0,0]
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    st.pyplot(fig1)
+    
+else:
+    st.error(f'Error: {response.status_code}')
 
 
 
--- analytics/views.py
+++ analytics/views.py
@@ -1,41 +1,79 @@
 from django.shortcuts import render,HttpResponse
 from django.http import JsonResponse
-import requests
+# import requests
 import json
 from django.views.decorators.csrf import csrf_exempt
 
 import csv
+from django.db.models import Sum
+from analytics.models import CustomerData
 
-
-# Create your views here.
-@csrf_exempt
-def analytics(request):
+# # Create your views here.
+# @csrf_exempt
+# def analytics(request):
     
-    if request.method == "GET":
-        file = open('/home/gopikrishna/Taigo/analytics_env/toucan_analytics/Data/data_for_database.csv',mode='r')
-        data = csv.reader(file)
-        dL = list(data)
-        dataList = dL[1:]
-        for row in dataList:
-            customerid = row[1]
-            category = row[2]
-            modeOfPayment = row[3]
-            amount = row[4]
-            date = row[5]
-            break
+#     if request.method == "GET":
+#         file = open('/home/jahnavi/Toucan_analytics_P1/toucan_analytics/Data/data_for_database.csv',mode='r')
+#         data = csv.reader(file)
+#         dL = list(data)
+#         dataList = dL[1:]
+#         for row in dataList:
+#             customerid = row[1]
+#             category = row[2]
+#             modeOfPayment = row[3]
+#             amount = row[4]
+#             date = row[5]
+#             break
 
-        dictt = {
+#         dictt = {
             
-            "customerid" : customerid,
-            "category" : category,
-            "mode" : modeOfPayment,
-            "amount" : amount,
-            "date" : date
-        }
-        file.close()
-        return JsonResponse(dictt)
+#             "customerid" : customerid,
+#             "category" : category,
+#             "mode" : modeOfPayment,
+#             "amount" : amount,
+#             "date" : date
+#         }
+#         file.close()
+#         return JsonResponse(dictt)
     
-    return HttpResponse("wow")
+#     return HttpResponse("wow")
 
 def index(request):
     return HttpResponse("index")
+
+def pie(request):
+    grouped_data = CustomerData.objects.values('category').annotate(sum_field=Sum('amount_spent'))
+
+# For PIE CHART
+    labels = []
+    total = []
+    sizes = []
+    for entry in grouped_data:
+        labels.append(entry['category']) 
+        total.append(entry['sum_field'])
+
+    sum_1 = sum(total)
+
+    for i in range(len(total)):
+        per = (total[i]/sum_1)*100
+        sizes.append(per)
+
+    response_data = {
+            "labels" : labels,
+            "sizes" : sizes,
+        }
+    return JsonResponse(response_data)
+
+@csrf_exempt
+def analytics(request):
+    if request.method == "GET":
+        type = request.GET.get('type')
+        if type == "pie":
+            response = pie(request)
+            return response
+        else:
+            return HttpResponse("NOT PIE")
+    else:
+        return HttpResponse("something")
+
+
