210ada4a462a71cfa99958b2933513394cbd6943
jahnavichelamkuri <jahnavichelamkuri@gmail.com>
2023-06-01 12:21:43 +0530

    created barchart for EMI payments

--- StreamLit/analytics.py
+++ StreamLit/analytics.py
@@ -62,3 +62,34 @@ if response.status_code == 200:
 else:
     st.error(f'Error: {response.status_code}')
 
+emi_list =[]
+customer_list =[]
+
+
+
+if st.button('GET'):
+
+    get_method=requests.get('http://127.0.0.1:8000/emi/')
+    if get_method.status_code==200:
+        data=get_method.json()
+        for item in data:
+            emi_list.append(item['EMI_paid_on_time'])
+            customer_list.append(item['total_customers'])
+    else:
+        st.write(get_method.status_code)
+
+   
+source = pd.DataFrame({
+    'EMI Paid On Time': emi_list,
+    'Number of customers': customer_list
+ })
+
+#user_colour = st.color_picker(label='Choose a colour for your plot')
+
+bar_chart = alt.Chart(source).mark_bar().encode(
+    x='EMI Paid On Time:O',
+    y='Number of customers:Q',
+    # c=user_colour,
+)
+
+st.altair_chart(bar_chart, use_container_width=True)
